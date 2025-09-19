a65c5311dc46c5196a713f614527d78e07817645
rodrigues.cco <bielcreations123@gmail.com>
2025-09-19 15:42:38 -0300

    removed MORE stuff

--- streamlit_calendar/frontend/src/components/Calendar.tsx
+++ streamlit_calendar/frontend/src/components/Calendar.tsx
@@ -2,9 +2,7 @@ import "../styles/Calendar.css"
 
 import {
     CalendarOptions,
-    EventApi,
     EventSourceInput,
-    ViewApi,
 } from "@fullcalendar/core"
 import FullCalendar from "@fullcalendar/react"
 
@@ -18,9 +16,6 @@ import React, { useRef } from "react"
 import { Streamlit, withStreamlitConnection } from "streamlit-component-lib"
 import { ComponentProps } from "streamlit-component-lib/dist/StreamlitReact"
 import styled from "styled-components"
-import {
-  Callback
-} from "../types/Calendar.type"
 
 const ENABLED_PLUGINS = [
   dayGridPlugin,
@@ -35,27 +30,12 @@ type Props = ComponentProps<{
   events?: EventSourceInput
   options?: CalendarOptions
   custom_css?: string
-  callbacks?: Callback[]
 }>
 
 const CalendarFC: React.FC<Props> = ({
-  args: { events, options, custom_css, callbacks },
+  args: { events, options, custom_css },
 }) => {
   const calendarRef = useRef<FullCalendar>(null)
-
-  // Use a WeakMap to associate elements with their listeners
-  const mouseEnterListeners = React.useRef(new WeakMap<HTMLElement, EventListener>()).current;
-
-  // This is called by FullCalendar for each event after it is mounted
-  const handleEventDidMount = (info: { event: EventApi; el: HTMLElement; view: ViewApi }) => {
-    if (callbacks?.includes("eventMouseEnter")) {
-      // Remove any previous listener to avoid duplicates
-      const prevListener = mouseEnterListeners.get(info.el);
-      if (prevListener) {
-        info.el.removeEventListener("mouseenter", prevListener);
-      }
-    }
-  }
 
   React.useEffect(() => {
     Streamlit.setFrameHeight()
@@ -68,9 +48,6 @@ const CalendarFC: React.FC<Props> = ({
         plugins={ENABLED_PLUGINS}
         events={events}
         locale={ptBrLocale}
-        eventDidMount={
-          callbacks?.includes("eventMouseEnter") ? handleEventDidMount : undefined
-        }
         {...options}
       />
     </FullCalendarWrapper>
